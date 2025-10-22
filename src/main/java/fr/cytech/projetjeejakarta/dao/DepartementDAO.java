dddd48ed1123e76cad5144399b8cd221d934c7f5
soufiane_fikri <105205326+soufianefikri@users.noreply.github.com>
2025-10-22 14:46:12 +0200

    Add files via upload

---
+++ src/main/java/fr/cytech/projetjeejakarta/dao/DepartementDAO.java
@@ -0,0 +1,36 @@
+package fr.cytech.projetmodel.dao;
+
+import fr.cytech.projetmodel.model.Departement;
+import fr.cytech.projetmodel.model.Employe;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.Persistence;
+
+public class DepartementDAO {
+    private EntityManagerFactory sessionFactory;
+
+    public DepartementDAO() {
+        sessionFactory = Persistence.createEntityManagerFactory("Projet");
+    }
+
+    public Departement fetchDepartement(int id_departement){
+        EntityManager em = sessionFactory.createEntityManager();
+        em.getTransaction().begin();
+        try {
+            Departement departement =(Departement) em.createQuery("select e from Departement e where e.id = :id_departement")
+                    .setParameter("id_departement",id_departement)
+                    .getSingleResult();
+
+            return departement;
+
+        }catch (NoResultException ex){
+            System.out.println("No departement found with id = " + id_departement);
+            return null;
+        }
+
+
+
+
+    }
+}
---
+++ src/main/java/fr/cytech/projetjeejakarta/dao/EmployeDAO.java
@@ -0,0 +1,35 @@
+package fr.cytech.projetmodel.dao;
+
+import fr.cytech.projetmodel.model.Employe;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.Persistence;
+
+public class EmployeDAO {
+    private EntityManagerFactory sessionFactory;
+
+    public EmployeDAO() {
+        sessionFactory = Persistence.createEntityManagerFactory("Projet");
+    }
+
+    public Employe fetchEmploye(int id_employe){
+        EntityManager em = sessionFactory.createEntityManager();
+        em.getTransaction().begin();
+        try {
+            Employe employe =(Employe) em.createQuery("select e from Employe e where e.id = :id_employe")
+                    .setParameter("id_employe",id_employe)
+                    .getSingleResult();
+
+            return employe;
+
+        }catch (NoResultException ex){
+            System.out.println("No cinema found with id = " + id_employe);
+            return null;
+        }
+
+
+
+
+    }
+}
